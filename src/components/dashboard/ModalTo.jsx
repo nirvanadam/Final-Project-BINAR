12ed5f09e96c8215439b541d63f47506f9a6d3b3
edwinvv12 <125728662+edwinvv12@users.noreply.github.com>
2023-06-20 01:32:05 +0700

    Modal-To (#16)

--- src/components/dashboard/ModalTo.jsx
+++ src/components/dashboard/ModalTo.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Select from "react-select";
+import axios from "axios";
 
 function ModalTo({ action, onDataSubmit }) {
   const [inputValue, SetInputValueTo] = useState();
@@ -7,17 +8,39 @@ function ModalTo({ action, onDataSubmit }) {
     SetInputValueTo(inputValue.label);
   };
 
-  const options = [
-    { value: "jakarta", label: "Jakarta" },
-    { value: "bandung", label: "Bandung" },
-    { value: "surabaya", label: "Surabaya" },
-  ];
+  // const options = [
+  //   { value: "jakarta", label: "Jakarta" },
+  //   { value: "bandung", label: "Bandung" },
+  //   { value: "surabaya", label: "Surabaya" },
+  // ];
 
   const handleSubmit = (event) => {
     event.preventDefault();
     onDataSubmit(inputValue);
     action();
   };
+
+  // Fetch API Logic
+  const [data, setData] = useState(null);
+
+  const url = "https://final-project-develop.up.railway.app/airport";
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(url);
+      setData(response.data.data);
+      setLoading(false);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [url]);
+
+  console.log(JSON.stringify(data));
+  // Fetch API Logic End
 
   return (
     // ToModal
@@ -35,12 +58,14 @@ function ModalTo({ action, onDataSubmit }) {
             onSubmit={handleSubmit}
             className="w-full flex flex-col gap-2 my-5"
           >
-            <Select
-              options={options}
-              value={options.value}
-              onChange={handleChange}
-              placeholder="Masukan Kota Tujuan"
-            />
+            {data && (
+              <Select
+                options={data}
+                value={data.value}
+                onChange={handleChange}
+                placeholder="Masukan Kota"
+              />
+            )}
           </form>
         </div>
       </div>
