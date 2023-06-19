6a40521a6e35dee101fb3d7c3a95d309d09fed78
nirvanadam <nirvan.pramudhyta@gmail.com>
2023-06-19 22:22:16 +0700

    consume API flight data

--- src/components/dashboard/ModalFrom.jsx
+++ src/components/dashboard/ModalFrom.jsx
@@ -1,5 +1,6 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import Select from "react-select";
+import axios from "axios";
 
 function ModalFrom({ close, onDataSubmit }) {
   const [inputValue, setInputValue] = useState(null);
@@ -7,11 +8,11 @@ function ModalFrom({ close, onDataSubmit }) {
     setInputValue(inputValue.label);
   };
 
-  const data = [
-    { value: "jakarta", label: "Jakarta" },
-    { value: "bandung", label: "Bandung" },
-    { value: "surabaya", label: "Surabaya" },
-  ];
+  // const option = [
+  //   { value: "jakarta", label: "Jakarta" },
+  //   { value: "bandung", label: "Bandung" },
+  //   { value: "surabaya", label: "Surabaya" },
+  // ];
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -19,29 +20,39 @@ function ModalFrom({ close, onDataSubmit }) {
     close();
   };
 
+  // Fetch API Logic
+  const [data, setData] = useState(null);
+
+  const url = "https://final-project-develop.up.railway.app/airport";
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(url);
+      setData(response.data.data);
+      setLoading(false);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [url]);
+
+  console.log(JSON.stringify(data));
+  // Fetch API Logic End
+
   return (
     // {FromModal}
 
     <div className="fixed w-full h-[80%] bottom-0 z-50 bg-white sm:bg-slate-500 sm:bg-opacity-80 sm:h-screen sm:w-screen">
       <div className="flex w-full h-full sm:justify-center sm:items-center">
         <div className="relative w-full sm:w-1/3 sm:bg-white sm:rounded-md flex flex-col px-5 pt-8 gap-8">
-          <button
-            onClick={close}
-            className="absolute top-0 right-5 text-[28px] font-semibold"
-          >
+          <button onClick={close} className="absolute top-0 right-5 text-[28px] font-semibold">
             x
           </button>
-          <form
-            onSubmit={handleSubmit}
-            className="w-full flex flex-col gap-2 my-5"
-          >
-            <Select
-
-              options={data}
-              value={data.value}
-              onChange={handleChange}
-              placeholder="Masukan Kota"
-            />
+          <form onSubmit={handleSubmit} className="w-full flex flex-col gap-2 my-5">
+            {data && <Select options={data} value={data.value} onChange={handleChange} placeholder="Masukan Kota" />}
           </form>
         </div>
       </div>
