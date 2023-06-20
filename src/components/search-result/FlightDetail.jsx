36bb4fab2dce53688a95eb52e9c19d2b8a5561b2
edwinvv12 <125728662+edwinvv12@users.noreply.github.com>
2023-06-20 23:09:46 +0700

    Edwin api (#17)
    * Modal-To
    
    * flight result
    
    ---------
    
    Co-authored-by: Nirvan Adam Pramudhyta <88652003+nirvanadam@users.noreply.github.com>

--- src/components/search-result/FlightDetail.jsx
+++ src/components/search-result/FlightDetail.jsx
@@ -1,12 +1,25 @@
 import React from "react";
 
 function FlightDetail(props) {
-  const { display } = props;
+  const {
+    display,
+    flightDate,
+    departureAirport,
+    arrivalAirport,
+    departureTime,
+    arrivalTime,
+    seatClass,
+    airlineCode,
+    airplaneID,
+    Informasi1,
+    Informasi2,
+    Informasi3,
+  } = props;
 
   return (
     <div className={`${display} mx-4 mt-3`}>
       <h1 className="lg:hidden mb-3 font-bold">
-        Jakarta - Melbourne <span>4h 0m</span>
+        {departureAirport} - {arrivalAirport} <span>{departureTime}</span>
       </h1>
 
       <h1 className="hidden lg:block mb-3 font-bold">Detail Penerbangan</h1>
@@ -16,13 +29,15 @@ function FlightDetail(props) {
         <div className="flex flex-col">
           <div className="flex justify-between items-center">
             <div className="flex flex-col">
-              <h1 className="font-bold">07:00</h1>
-              <h1 className="text-sm font-semibold">3 Maret 2023</h1>
+              <h1 className="font-bold">{departureTime}</h1>
+              <h1 className="text-sm font-semibold">{flightDate}</h1>
             </div>
             <h1 className="text-sm font-semibold">Keberangkatan</h1>
           </div>
 
-          <h1 className="text-sm font-semibold">Soekarno Hatta - Terminal 1A Domestik</h1>
+          <h1 className="text-sm font-semibold">
+            {departureAirport} - {arrivalAirport}
+          </h1>
         </div>
         {/* Keberangkatan End */}
 
@@ -31,15 +46,19 @@ function FlightDetail(props) {
           <img src="/icons/airline_logo.svg" alt="" />
           <div className="flex flex-col gap-2">
             <div className="flex flex-col">
-              <h1 className="font-bold">Jet Air - Economy</h1>
-              <h1 className="font-bold">JT - 203</h1>
+              <h1 className="font-bold">
+                {airlineCode} - {seatClass}
+              </h1>
+              <h1 className="font-bold">
+                {airlineCode} - {airplaneID}
+              </h1>
             </div>
 
             <div className="flex flex-col">
               <h1 className="font-bold">Informasi</h1>
-              <h1 className="font-semibold">Baggage 20kg</h1>
-              <h1 className="font-semibold">Cabin baggage 7kg</h1>
-              <h1 className="font-semibold">In Flight Entertainment</h1>
+              <h1 className="font-semibold">Baggage {Informasi1}</h1>
+              <h1 className="font-semibold">Cabin baggage {Informasi2}</h1>
+              <h1 className="font-semibold">Capacity {Informasi3}</h1>
             </div>
           </div>
         </div>
@@ -49,13 +68,13 @@ function FlightDetail(props) {
         <div className="flex flex-col">
           <div className="flex justify-between items-center">
             <div className="flex flex-col">
-              <h1 className="font-bold">11:00</h1>
-              <h1 className="text-sm font-semibold">3 Maret 2023</h1>
+              <h1 className="font-bold">{arrivalTime}</h1>
+              <h1 className="text-sm font-semibold">{flightDate}</h1>
             </div>
             <h1 className="text-sm font-semibold">Kedatangan</h1>
           </div>
 
-          <h1 className="text-sm font-semibold">Melbourne International Airport</h1>
+          <h1 className="text-sm font-semibold">{arrivalAirport}</h1>
         </div>
         {/* Kedatangan End */}
       </div>
--- src/components/search-result/FlightResult.jsx
+++ src/components/search-result/FlightResult.jsx
@@ -9,36 +9,100 @@ function FlightResult() {
     setDetailBtnSts(!detailBtnSts);
   };
 
+
+  const [data, setData] = useState();
+
+  const url = `https://final-project-develop.up.railway.app/api/flight`;
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(url);
+      console.log(response);
+      setData(response.data.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [url]);
+
+  function formatCurrency(amount) {
+    const formatter = new Intl.NumberFormat("id-ID", {
+      style: "currency",
+      currency: "IDR",
+    });
+
+    return formatter.format(amount);
+  }
+
   return (
     <div className="flex flex-col gap-5 mx-4">
       {/* Flight Information Container */}
-      <div className="flex flex-col gap-2 p-4 border border-gray-300 rounded-lg shadow-md">
-        <div className="flex flex-col ">
-          {/* From, Duration, To, Price */}
-          <div className="lg:order-3 flex justify-between ">
-            <div className="flex gap-3">
-              {/* From Information */}
-              <div className="flex flex-col items-center">
-                <h1 className="font-bold">07:00</h1>
-                <h1 className="text-sm font-semibold">JKT</h1>
-              </div>
-              {/* From Information End */}
+      {data?.slice(0, 100).map((datas, index) => (
+        <div
+          key={index}
+          className="flex flex-col gap-2 p-4 border border-gray-300 rounded-lg shadow-md"
+        >
+          <div className="flex flex-col ">
+            {/* From, Duration, To, Price */}
+            <div className="lg:order-3 flex justify-between ">
+              <div className="flex gap-3">
+                {/* From Information */}
+                <div className="flex flex-col items-center">
+                  <h1 className="font-bold">{datas.departure_time}</h1>
+                  <h1 className="text-sm font-semibold">
+                    {datas.departure_airport}
+                  </h1>
+                </div>
+                {/* From Information End */}
 
-              {/* Duration */}
-              <div className="flex flex-col w-20 lg:w-60 gap-1 items-center">
-                <h1 className="text-xs font-semibold">4h 0m</h1>
-                <span className="h-[1px] w-full bg-black"></span>
-                <h1 className="text-xs font-semibold">Direct</h1>
-              </div>
-              {/* Duration End */}
+                {/* Duration */}
+                <div className="flex flex-col w-20 lg:w-60 gap-1 items-center">
+                  <h1 className="text-xs font-semibold">
+                    {datas.flight_duration}
+                  </h1>
+                  <span className="h-[1px] w-full bg-black"></span>
+                  <h1 className="text-xs font-semibold">Direct</h1>
+                </div>
+                {/* Duration End */}
 
-              {/* To Information */}
-              <div className="flex flex-col items-center">
-                <h1 className="font-bold">11:00</h1>
-                <h1 className="text-sm font-semibold">MLB</h1>
+                {/* To Information */}
+                <div className="flex flex-col items-center">
+                  <h1 className="font-bold">{datas.arrival_time}</h1>
+                  <h1 className="text-sm font-semibold">
+                    {datas.arrival_airport}
+                  </h1>
+                </div>
+                {/* To Information End */}
               </div>
-              {/* To Information End */}
+
+              {/* Price */}
+              <div className="flex flex-col items-end gap-1">
+                <h1 className="text-lg w-fit font-bold ">
+                  {formatCurrency(datas.price)}
+                </h1>
+                <button className="hidden lg:inline w-fit bg-primary text-white font-medium px-9 py-1 rounded-lg">
+                  Pilih
+                </button>
+              </div>
+              {/* Price End */}
             </div>
+            {/* From, Duration, To, Price End */}
+
+            <span className="lg:order-2 bg-gray-400 my-2 w-full h-[1px]"></span>
+
+            {/*  Airline - Seat Class & Detail Button */}
+            <div className="lg:order-1 flex justify-between items-center">
+              {/* Seat Class Info */}
+              <div className="flex items-center gap-2">
+                <img src="/icons/airline_logo.svg" alt="" />
+                <div className="flex flex-col gap-1">
+                  <h1 className="text-sm font-semibold">
+                    {datas.airline_code} - {datas.class}
+                  </h1>
+                  <img src="/icons/luggage_icon.svg" alt="" className="w-5" />
+                </div>
 
             {/* Price */}
             <div className="flex flex-col items-end gap-1">
@@ -61,9 +125,28 @@ function FlightResult() {
               <div className="flex flex-col gap-1">
                 <h1 className="text-sm font-semibold">Jet Air- Economy</h1>
                 <img src="/icons/luggage_icon.svg" alt="" className="w-5" />
+
               </div>
+              {/* Seat Class Info End */}
+
+              {/* Detail Button */}
+              <Link
+                to={"/flight-detail"}
+                className="flex lg:hidden justify-center items-center border border-gray-300 rounded-full p-1"
+              >
+                <img src="/icons/chevron_down.svg" alt="" className="w-5" />
+              </Link>
+
+              <button
+                type="button"
+                onClick={detailButton}
+                className="hidden lg:flex justify-center items-center border border-gray-300 rounded-full p-1"
+              >
+                <img src="/icons/chevron_down.svg" alt="" className="w-5" />
+              </button>
+              {/* Detail Button End */}
             </div>
-            {/* Seat Class Info End */}
+            {/*  Airline - Seat Class & Detail Button End */}
 
             {/* Detail Button */}
             <Link
@@ -82,11 +165,23 @@ function FlightResult() {
             </button>
             {/* Detail Button End */}
           </div>
-          {/*  Airline - Seat Class & Detail Button End */}
-        </div>
 
-        <FlightDetail display={detailBtnSts ? "block" : "hidden"} />
-      </div>
+          <FlightDetail
+            flightDate={datas.flight_date}
+            departureAirport={datas.departure_airport}
+            arrivalAirport={datas.arrival_airport}
+            airlineCode={datas.airline_code}
+            seatClass={datas.class}
+            departureTime={datas.departure_time}
+            arrivalTime={datas.arrivalTime}
+            airplaneID={datas.airplane_id}
+            Informasi1={datas.free_baggage}
+            Informasi2={datas.cabin_baggage}
+            Informasi3={datas.capacity}
+            display={detailBtnSts ? "block" : "hidden"}
+          />
+        </div>
+      ))}
       {/* Flight Information Container End */}
     </div>
   );
