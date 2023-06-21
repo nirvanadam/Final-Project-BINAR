e3de55b14a333e8ecef2b719ef9e39954e0fb185
edwinvv12 <125728662+edwinvv12@users.noreply.github.com>
2023-06-21 19:43:51 +0700

    Edwin debugging (#18)
    * flight result
    
    * Debugging completed
    
    ---------
    
    Co-authored-by: Nirvan Adam Pramudhyta <88652003+nirvanadam@users.noreply.github.com>

--- src/components/search-result/FlightResult.jsx
+++ src/components/search-result/FlightResult.jsx
@@ -1,12 +1,31 @@
-import React, { useState, useEffect } from "react";
+import React, { useState , useEffect } from "react";
 import { Link } from "react-router-dom";
 import FlightDetail from "./FlightDetail";
 import axios from "axios";
 
 function FlightResult() {
   const [detailBtnSts, setDetailBtnSts] = useState(false);
-  const detailButton = () => {
-    setDetailBtnSts(!detailBtnSts);
+  // const detailButton = () => {
+  //   setDetailBtnSts(!detailBtnSts);
+  // };
+
+  const detailButton = (index) => {
+    setDetailBtnSts((prevState) => ({
+      ...prevState,
+      [index]: !prevState[index]
+    }));
+  };
+
+  const [data , setData] = useState();
+  const url = `https://final-project-develop.up.railway.app/api/flight`;
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(url);
+      console.log(response);
+      setData(response.data.data);
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   const [data, setData] = useState();
@@ -37,39 +56,40 @@ function FlightResult() {
   return (
     <div className="flex flex-col gap-5 mx-4">
       {/* Flight Information Container */}
-      {data?.slice(0, 20).map((datas, index) => (
-        <div key={index} className="flex flex-col gap-2 p-4 border border-gray-300 rounded-lg shadow-md">
-          <div className="flex flex-col ">
-            {/* From, Duration, To, Price */}
-            <div className="lg:order-3 flex justify-between ">
-              <div className="flex gap-3">
-                {/* From Information */}
-                <div className="flex flex-col items-center">
-                  <h1 className="font-bold">{datas.departure_time}</h1>
-                  <h1 className="text-sm font-semibold">{datas.departure_airport}</h1>
-                </div>
-                {/* From Information End */}
+      {data?.slice(0, 20 ).map((datas, index)=> (
 
-                {/* Duration */}
-                <div className="flex flex-col w-20 lg:w-60 gap-1 items-center">
-                  <h1 className="text-xs font-semibold">{datas.flight_duration}</h1>
-                  <span className="h-[1px] w-full bg-black"></span>
-                  <h1 className="text-xs font-semibold">Direct</h1>
-                </div>
-                {/* Duration End */}
+      <div 
+      key={index}
+      className="flex flex-col gap-2 p-4 border border-gray-300 rounded-lg shadow-md">
+        <div className="flex flex-col ">
+          {/* From, Duration, To, Price */}
+          <div className="lg:order-3 flex justify-between ">
+            <div className="flex gap-3">
+              {/* From Information */}
+              <div className="flex flex-col items-center">
+                <h1 className="font-bold">{datas.departure_time}</h1>
+                <h1 className="text-sm font-semibold">{datas.departure_airport}</h1>
+              </div>
+              {/* From Information End */}
 
-                {/* To Information */}
-                <div className="flex flex-col items-center">
-                  <h1 className="font-bold">{datas.arrival_time}</h1>
-                  <h1 className="text-sm font-semibold">{datas.arrival_airport}</h1>
-                </div>
-                {/* To Information End */}
+              {/* Duration */}
+              <div className="flex flex-col w-20 lg:w-60 gap-1 items-center">
+                <h1 className="text-xs font-semibold">{datas.flight_duration}</h1>
+                <span className="h-[1px] w-full bg-black"></span>
+                <h1 className="text-xs font-semibold">Direct</h1>
+              </div>
+              {/* Duration End */}
+
+              {/* To Information */}
+              <div className="flex flex-col items-center">
+                <h1 className="font-bold">{datas.arrival_time}</h1>
+                <h1 className="text-sm font-semibold">{datas.arrival_airport}</h1>
               </div>
 
-              {/* Price */}
-              <div className="flex flex-col items-end gap-1">
-                <h1 className="text-lg w-fit font-bold ">{formatCurrency(datas.price)}</h1>
-                <button className="hidden lg:inline w-fit bg-primary text-white font-medium px-9 py-1 rounded-lg">Pilih</button>
+            {/* Price */}
+            <div className="flex flex-col items-end gap-1">
+              <h1 className="text-lg w-fit font-bold ">{formatCurrency(datas.price)}</h1>
+              <button className="hidden lg:inline w-fit bg-primary text-white font-medium px-9 py-1 rounded-lg">Pilih</button>
               </div>
               {/* Price End */}
             </div>
@@ -77,16 +97,14 @@ function FlightResult() {
 
             <span className="lg:order-2 bg-gray-400 my-2 w-full h-[1px]"></span>
 
-            {/*  Airline - Seat Class & Detail Button */}
-            <div className="lg:order-1 flex justify-between items-center">
-              {/* Seat Class Info */}
-              <div className="flex items-center gap-2">
-                <img src="/icons/airline_logo.svg" alt="" />
-                <div className="flex flex-col gap-1">
-                  <h1 className="text-sm font-semibold">
-                    {datas.airline_code} - {datas.class}
-                  </h1>
-                  <img src="/icons/luggage_icon.svg" alt="" className="w-5" />
+          {/*  Airline - Seat Class & Detail Button */}
+          <div className="lg:order-1 flex justify-between items-center">
+            {/* Seat Class Info */}
+            <div className="flex items-center gap-2">
+              <img src="/icons/airline_logo.svg" alt="" />
+              <div className="flex flex-col gap-1">
+                <h1 className="text-sm font-semibold">{datas.airline_code} - {datas.class}</h1>
+                <img src="/icons/luggage_icon.svg" alt="" className="w-5" />
                 </div>
               </div>
               {/* Seat Class Info End */}
@@ -96,29 +114,29 @@ function FlightResult() {
                 <img src="/icons/chevron_down.svg" alt="" className="w-5" />
               </Link>
 
-              <button type="button" onClick={detailButton} className="hidden lg:flex justify-center items-center border border-gray-300 rounded-full p-1">
-                <img src="/icons/chevron_down.svg" alt="" className="w-5" />
-              </button>
-              {/* Detail Button End */}
+            <button type="button" onClick={()=> detailButton(index)} className="hidden lg:flex justify-center items-center border border-gray-300 rounded-full p-1">
+              <img src="/icons/chevron_down.svg" alt="" className="w-5" />
+            </button>
+            {/* Detail Button End */}
             </div>
             {/*  Airline - Seat Class & Detail Button End */}
           </div>
 
-          <FlightDetail
-            flightDate={datas.flight_date}
-            departureAirport={datas.departure_airport}
-            arrivalAirport={datas.arrival_airport}
-            airlineCode={datas.airline_code}
-            seatClass={datas.class}
-            departureTime={datas.departure_time}
-            arrivalTime={datas.arrivalTime}
-            airplaneID={datas.airplane_id}
-            Informasi1={datas.free_baggage}
-            Informasi2={datas.cabin_baggage}
-            Informasi3={datas.capacity}
-            display={detailBtnSts ? "block" : "hidden"}
-          />
-        </div>
+        <FlightDetail 
+        flightDate={datas.flight_date}
+        departureAirport={datas.departure_airport}
+        arrivalAirport={datas.arrival_airport}
+        airlineCode={datas.airline_code}
+        seatClass={datas.class}
+        departureTime={datas.departure_time}
+        arrivalTime={datas.arrivalTime}
+        airplaneID={datas.airplane_id}
+        Informasi1={datas.free_baggage}
+        Informasi2={datas.cabin_baggage}
+        Informasi3={datas.capacity}
+        display={detailBtnSts[index] ? "block" : "hidden"} />
+      </div>
+
       ))}
       {/* Flight Information Container End */}
     </div>
