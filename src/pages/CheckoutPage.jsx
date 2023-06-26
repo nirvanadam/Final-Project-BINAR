31cb63ed4644ea552a0bfe69373f536ced4df466
Nirvan Adam Pramudhyta <88652003+nirvanadam@users.noreply.github.com>
2023-06-27 01:42:52 +0700

    tambah fungsi checkout (#23)

--- src/pages/CheckoutPage.jsx
+++ src/pages/CheckoutPage.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import BioDataPemesan from "../components/checkout/BioDataPemesan";
 import BioDataPenumpang from "../components/checkout/BioDataPenumpang";
 import Navbar from "../components/Navbar";
