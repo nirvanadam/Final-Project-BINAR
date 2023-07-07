0036f6bdbce98f499a3b4e05778ffc7b46821bae
edwinvv12 <125728662+edwinvv12@users.noreply.github.com>
2023-07-07 13:59:45 +0700

    Fitur fixing (#52)
    * success , resend otp
    
    * riwayat
    
    * fixing history and payment from history
    
    * fixingSearch History

--- src/components/ModalBookingCode.jsx
+++ src/components/ModalBookingCode.jsx
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 
-function ModalBookingCode({ close }) {
+function ModalBookingCode({ close, set, data }) {
+  const [code, setCode] = useState();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const dataSearch = data.find((data) => data.booking_code == code);
+    set(dataSearch);
+    close();
+  };
   return (
     <div className="fixed w-full h-[80%] bottom-0 z-50 bg-white sm:bg-slate-500 sm:bg-opacity-80 sm:h-screen sm:w-screen">
       <div className="flex w-full h-full sm:justify-center sm:items-center">
@@ -11,12 +18,16 @@ function ModalBookingCode({ close }) {
           >
             x
           </button>
-          <form className="w-full flex flex-col gap-2 mt-5 my-5">
+          <form
+            onSubmit={handleSubmit}
+            className="w-full flex flex-col gap-2 mt-5 my-5"
+          >
             <div className="relative w-full lg:flex items-center rounded-lg border-2 px-2.5 py-2">
               <input
                 className="w-full bg-transparent rounded-lg px-5 py-2 outline-none"
                 placeholder="Masukan Booking Code"
                 type="text"
+                onChange={(e) => setCode(e.target.value)}
               />
               <button className="absolute right-5 mt-2 lg:mt-0">
                 <img
