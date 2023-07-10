4a440b203a2e36660f5a666aa749f0d5e97d7921
nirvanadam <nirvan.pramudhyta@gmail.com>
2023-07-11 01:24:47 +0700

    hapus rp di destinasi favorite

--- src/components/dashboard/DestinasiFavorit.jsx
+++ src/components/dashboard/DestinasiFavorit.jsx
@@ -99,17 +99,9 @@ function DestinasiFavorit() {
   const DestinasiCard = () => {
     return data?.map((destinasi, index) => {
       return (
-        <div
-          key={index}
-          className="bg-zinc-100 relative inline-flex flex-col lg:mt-3 mr-5 lg:mr-0 overflow-hidden group rounded-xl shadow-md cursor-default transition-all duration-[0.4s] hover:-translate-y-3 whitespace-normal	"
-        >
+        <div key={index} className="bg-zinc-100 relative inline-flex flex-col lg:mt-3 mr-5 lg:mr-0 overflow-hidden group rounded-xl shadow-md cursor-default transition-all duration-[0.4s] hover:-translate-y-3 whitespace-normal	">
           <div className="overflow-hidden relative h-[200px] before:content-[''] before:absolute before:top-0 before:bottom-0 before:left-0 before:right-0 before:bg-gradient-to-t from-black from-5% before:z-10">
-            <img
-              key={index}
-              src={`https://source.unsplash.com/1600x900/?kota&indnesia&${destinasi.data.arrival}`}
-              alt=""
-              className="absolute transition-all duration-[0.4s] group-hover:scale-125"
-            />
+            <img key={index} src={`https://source.unsplash.com/1600x900/?kota&indnesia&${destinasi.data.arrival}`} alt="" className="absolute transition-all duration-[0.4s] group-hover:scale-125" />
             <div className="absolute bottom-3 left-3 z-20 flex flex-col gap-1">
               <h1 className="text-white font-white">
                 {destinasi.data.departure} - {destinasi.data.arrival}
@@ -122,28 +114,17 @@ function DestinasiFavorit() {
                   <img src="/icons/star-icon.svg" alt="" />
                   <img src="/icons/star-icon.svg" alt="" />
                 </div>
-                <h1 className="text-sm text-gray-400 font-medium">
-                  (5.0) Rating
-                </h1>
+                <h1 className="text-sm text-gray-400 font-medium">(5.0) Rating</h1>
               </div>
             </div>
           </div>
           <div className="flex flex-col items-center gap-4 p-6">
             <h1 className="text-xl font-semibold">
-              Rp {formatCurrency(destinasi.data.price)}
-              <span className="text-base text-gray-500 font-medium">
-                / Orang
-              </span>
+              {formatCurrency(destinasi.data.price)}
+              <span className="text-base text-gray-500 font-medium">/ Orang</span>
             </h1>
-            <h1 className="text-center text-sm text-gray-500 font-medium">
-              Our travels are different from all the others! We love our
-              business and want to see happy customers, so we always do our best
-              for your comfort!
-            </h1>
-            <button
-              onClick={() => handleOnClick(destinasi.id)}
-              className="bg-white border border-gray-300 rounded-full px-10 py-3 text-sm font-semibold tracking-[2px] duration-300 transition-all hover:bg-primary hover:text-white"
-            >
+            <h1 className="text-center text-sm text-gray-500 font-medium">Our travels are different from all the others! We love our business and want to see happy customers, so we always do our best for your comfort!</h1>
+            <button onClick={() => handleOnClick(destinasi.id)} className="bg-white border border-gray-300 rounded-full px-10 py-3 text-sm font-semibold tracking-[2px] duration-300 transition-all hover:bg-primary hover:text-white">
               ORDER
             </button>
           </div>
@@ -159,11 +140,7 @@ function DestinasiFavorit() {
         return (
           <div className="bg-zinc-100 relative inline-flex flex-col lg:mt-3 mr-5 lg:mr-0 overflow-hidden group rounded-xl shadow-md cursor-default transition-all hover:-translate-y-3 whitespace-normal	">
             <div className="overflow-hidden relative h-[200px] before:content-[''] before:absolute before:top-0 before:bottom-0 before:left-0 before:right-0 before:bg-gradient-to-t from-black from-5% before:z-10">
-              <img
-                src={destinasi.foto}
-                alt=""
-                className="absolute transition-all duration-300 group-hover:scale-125"
-              />
+              <img src={destinasi.foto} alt="" className="absolute transition-all duration-300 group-hover:scale-125" />
               <div className="absolute bottom-3 left-3 z-20 flex flex-col gap-1">
                 <h1 className="text-white font-white">{destinasi.name}</h1>
                 <div className="flex gap-2">
@@ -174,27 +151,16 @@ function DestinasiFavorit() {
                     <img src="/icons/star-icon.svg" alt="" />
                     <img src="/icons/star-icon.svg" alt="" />
                   </div>
-                  <h1 className="text-sm text-gray-400 font-medium">
-                    (5.0) Rating
-                  </h1>
+                  <h1 className="text-sm text-gray-400 font-medium">(5.0) Rating</h1>
                 </div>
               </div>
             </div>
             <div className="flex flex-col items-center gap-4 p-6">
               <h1 className="text-xl font-semibold">
-                Rp {destinasi.price}{" "}
-                <span className="text-base text-gray-500 font-medium">
-                  / Orang
-                </span>
+                Rp {destinasi.price} <span className="text-base text-gray-500 font-medium">/ Orang</span>
               </h1>
-              <h1 className="text-center text-sm text-gray-500 font-medium">
-                Our travels are different from all the others! We love our
-                business and want to see happy customers, so we always do our
-                best for your comfort!
-              </h1>
-              <button className="bg-white border border-gray-300 rounded-full px-10 py-3 text-sm font-semibold tracking-[2px] duration-300 transition-all hover:bg-primary hover:text-white">
-                ORDER
-              </button>
+              <h1 className="text-center text-sm text-gray-500 font-medium">Our travels are different from all the others! We love our business and want to see happy customers, so we always do our best for your comfort!</h1>
+              <button className="bg-white border border-gray-300 rounded-full px-10 py-3 text-sm font-semibold tracking-[2px] duration-300 transition-all hover:bg-primary hover:text-white">ORDER</button>
             </div>
           </div>
         );
@@ -206,12 +172,8 @@ function DestinasiFavorit() {
       <section>
         <div className="relative overflow-hidden bg-[url('/images/edinburgh-poster.jpg')] bg-cover bg-fixed bg-center flex justify-center items-center h-[300px] lg:h-[600px] border border-red-50 before:content-[''] before:absolute before:bg-black before:top-0 before:bottom-0 before:left-0 before:right-0 before:opacity-40">
           <div className="flex flex-col items-center z-10">
-            <h1 className="text-lg lg:text-2xl text-white font-bold italic">
-              Get Your Ticket and Flight With Us
-            </h1>
-            <h1 className="text-4xl lg:text-9xl text-white font-bold">
-              Destination
-            </h1>
+            <h1 className="text-lg lg:text-2xl text-white font-bold italic">Get Your Ticket and Flight With Us</h1>
+            <h1 className="text-4xl lg:text-9xl text-white font-bold">Destination</h1>
           </div>
         </div>
 
@@ -219,9 +181,7 @@ function DestinasiFavorit() {
 
         {/* Filter Button End */}
 
-        <div className="whitespace-nowrap lg:whitespace-normal lg:grid grid-cols-3 gap-5 overflow-auto mt-5 mx-4 md:mx-8 lg:mx-28 py-5">
-          {region === "Semua" ? DestinasiCard() : FilteredCard()}
-        </div>
+        <div className="whitespace-nowrap lg:whitespace-normal lg:grid grid-cols-3 gap-5 overflow-auto mt-5 mx-4 md:mx-8 lg:mx-28 py-5">{region === "Semua" ? DestinasiCard() : FilteredCard()}</div>
       </section>
       {/* Tour Section End */}
 
